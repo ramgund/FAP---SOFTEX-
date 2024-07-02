50121bf68dbfebf1eaf572c5206909fa57275c97
nada <nadatambem>
2024-07-02 16:11:30 -0300

    Att do exercicio

--- exe05.js
+++ exe05.js
@@ -4,6 +4,8 @@ const rl = require('readline-sync')
 var rodadas = 0;
 var pontJ1 = 0;
 var pontJ2 = 0; 
+var palitosJ1 = 0;
+var palitosJ2 = 0;
 
 
 
@@ -15,27 +17,34 @@ function iniciarJogo () {
         rodadas++;
         console.log(`Rodada ${rodadas}`)
 
-        pontJ1 = Math.floor(Math.random() * 4)
-        pontJ2 = Math.floor(Math.random() * 4)
+        palitosJ1 = Math.floor(Math.random() * 4)
+        palitosJ2 = Math.floor(Math.random() * 4)
 
-        var soma = pontJ1 + pontJ2
+        console.log(`Palitos do jogador 1: ${palitosJ1}`);
+        console.log(`Palitos do jogador 2: ${palitosJ2}`)
+
+        var soma = palitosJ1 + palitosJ2
 
         var palpitej1 = parseFloat(rl.question("Digite a quantidade da soma de palitos (jogador1): "))
         var palpitej2 = parseFloat(rl.question("Digite a quantidade da soma de palitos (jogador2): "))
 
         if (palpitej1 == soma) {
             console.log("Jogador 1 ganhou!")
-            rodadas++
             pontJ1++
+            console.log(`Pontuação J1: ${pontJ1} | Pontuação J2: ${pontJ2}`)
         } else if (palpitej2 == soma) {
             console.log("Jogador 2 ganhou.")
-            rodadas++
             pontJ2++
-        } else if (palpitej1 != soma && palpitej2 != soma) {
-            console.log("Empate")
-            rodadas++
-        }
+            console.log(`Pontuação J1: ${pontJ1} | Pontuação J2: ${pontJ2}`)
+        } 
+
     }
-    
+
+   
+    if (pontJ1 > pontJ2) {
+        console.log("Jogador 1 ganhou!")
+    } else {
+        console.log("Jogador 2 ganhou!")
+    }   
 }
  iniciarJogo()
