06b80964004698779f939360c576f92fc0fa8c99
ramgund <vtwolmer9@gmail.com>
2024-07-11 13:47:53 -0300

    Delete index.js

--- ano_bissexto/index.js
+++
@@ -1,9 +0,0 @@
-const readline = require("readline-sync");
-
-let ano = readline.question("DIgite o ano: ");
-
-if ((ano % 100 !== 0 && ano % 4 === 0) || (ano % 400 === 0)) {
-    console.log("O ano é bissexto!")
-} else {
-    console.log("O ano não é bissexto!")
-}
